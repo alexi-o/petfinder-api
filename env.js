962c9c5347f4e2515831e715425bc328d22ba051
AlexiOHearn <alexiohearn@o.local>
2017-04-23 23:07:02 -0600

    testing heroku

---
+++ env.js
@@ -0,0 +1,6 @@
+'use strict'
+
+var petKey = "925c6202c203f971efa10681383dd089";
+
+module.exports = petKey;
+
---
+++ env2.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var gooKey = "AIzaSyB7CZMeH5ZlzmQLq_RlpWItqmXi44nM5Jg";
+
+module.exports = gooKey;
